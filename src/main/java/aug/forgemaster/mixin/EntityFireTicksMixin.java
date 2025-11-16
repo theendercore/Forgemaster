bce1d13ff20a4f9c567b76e90c78c9e97188c8d1
The Typhothanian <thetyphothanian@gmail.com>
2025-11-16 07:25:26 -0700

    tweaked entity fire ticks mixin

--- src/main/java/aug/forgemaster/mixin/EntityFireTicksMixin.java
+++ src/main/java/aug/forgemaster/mixin/EntityFireTicksMixin.java
@@ -6,18 +6,16 @@ import aug.forgemaster.item.ModItemComponentTypes;
 import aug.forgemaster.item.ModItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageSources;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
@@ -25,11 +23,21 @@ public abstract class EntityFireTicksMixin {
     @Unique
     public abstract boolean hasStatusEffect(RegistryEntry<StatusEffect> effect);
 
-    @Shadow
-    public abstract boolean damage(DamageSource source, float amount);
+    @ModifyArg(
+            method = "baseTick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"
+            ),
+            index = 1
+    )
+    private float increaseFireDamage(float amount) {
+        if (hasStatusEffect(ModEffects.SPARKED)) {
+            amount += 1.5f;
+        }
 
-    @Shadow
-    public abstract DamageSources getDamageSources();
+        return amount;
+    }
 
     @Inject(
             method = "baseTick",
@@ -41,10 +49,6 @@ public abstract class EntityFireTicksMixin {
     private void baseTick(CallbackInfo ci) {
         if ((Object) this instanceof LivingEntity living && living.getAttacker() != null) {
             ItemStack stack = living.getAttacker().getMainHandStack();
-
-            if (hasStatusEffect(ModEffects.SPARKED)) {
-                damage(getDamageSources().onFire(), 1.5f);
-            }
 
             if (stack.isOf(ModItems.ATTACCA)) {
                 stack.apply(ModItemComponentTypes.ATTACCA_CHARGE, 0, i -> MathHelper.clamp(i + 1, 0, AttaccaItem.MAX_CHARGE));
